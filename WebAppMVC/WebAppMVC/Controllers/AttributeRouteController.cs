ba497806f67187c81be4dbf3b96753200bbc769f
unknown <Administrator@SOFTDESK13.capricornlogistics.in>
2025-03-11 16:31:08 +0530

    Attribute Routing in MVC

---
+++ WebAppMVC/WebAppMVC/Controllers/AttributeRouteController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMVC.Controllers
+{
+    public class AttributeRouteController : Controller
+    {
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("About")]
+        [Route("About/Index")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("Details/{id?}")]
+        public int Details(int? id) // Nullable handling
+        {
+            return id ?? 3129; // IF Null then Show 3129 
+        }
+
+        // We give any name to Action method but view should give our HTML View path in View() Mthod
+        [Route("Payment")]
+        public IActionResult PaymentActionMethod()
+        { 
+            return View("~/Views/AttributeRoute/Pay.cshtml");
+        }
+
+
+    }
+}
