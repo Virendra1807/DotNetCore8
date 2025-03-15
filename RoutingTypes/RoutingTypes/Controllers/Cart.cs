a9f155b190de28a858b61f35c9702ba56988cf20
unknown <Administrator@SOFTDESK13.capricornlogistics.in>
2025-03-15 12:09:05 +0530

    Routing Types

---
+++ RoutingTypes/RoutingTypes/Controllers/Cart.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingTypes.Controllers
+{   
+    public class Cart : Controller
+    {
+        public IActionResult CartView()
+        {
+            return View();
+        }
+        public int Details(int id)
+        {
+            return id; 
+        }
+    }
+}
